87baa5acb5d0940723a24d762b80f6bc8c15e3c2
Nyanchl <rotdicm@gmail.com>
2024-02-19 15:35:57 +0900

    Rust学習中

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let mut guess = String::new();
+    // mutはmutableを定義してるらしい
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}", guess);
 }
